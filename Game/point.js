7ff4e98b8bca41e8df658ffb1354a5629d9be7b1
Sokha93 <sokhaaliev@gmail.com>
2020-02-25 14:23:37 +0400

    some changes

---
+++ Game/point.js
@@ -0,0 +1,13 @@
+const text = `Two elderly cruise ship passengers with coronavirus who were on Princess Cruises Diamond Princess
+ have died, according to Japan’s health ministry, the Associated Press has confirmed
+A health ministry official confirmed that they had been previously hospitalized in serious conditio
+n and had existing chronic diseases. The official spoke anonymously, citing office protocol
+Johns Hopkins Center for Systems Science and Engineering, which has built a map of coronaviru
+s data including cases, fatalities and recoveries, also cites two deaths from the Diamond Princess,
+ which brings the total death toll from the virus in Japan to three. Per the data, 621 cases of the
+ virus had been identified among the 3,711 quarantined passengers and crew, making the ship the site
+  of the most infections outside of China; one Diamond Princess passenger has recovered.
+According to Japanese broadcast outlet, NHK, the two Japanese cruise passengers who died from the
+ virus were an 87-year-old man and an 84-year-old woman.`
+ const phrases = text.split('\n')
+ console.log(phrases)
